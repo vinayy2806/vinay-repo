782482c11590b1c2b8ea7a63bcc68f33c594ddeb
vinay <vinay28623@gmail.com>
2025-01-22 18:17:47 +0530

    commit the terraform

---
+++ terraform/scrip.sh
@@ -0,0 +1,17 @@
+#!/bin//bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade 
+sudo yum install java-17 -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+sudo hostname admin-server
