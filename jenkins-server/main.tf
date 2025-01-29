cefb951f1753dc0f1cae2a7da7e5819ccf2c43d5
VINAY Y <vinay28623@gmail.com>
2025-01-29 18:06:57 +0530

    Update main.tf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ""
-    secret_key = ""
+    access_key = "AKIASE5KRGE4ZFBVILTM"
+    secret_key = "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"
 }
 
 resource "aws_vpc" "main" {
@@ -52,7 +52,7 @@ resource "aws_instance" "ec2" {
     ami = "ami-0df8c184d5f6ae949"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.subnet.id
-    key_name = "project"
+    key_name = "147"
     associate_public_ip_address = true
 
     tags = {
@@ -64,5 +64,5 @@ resource "aws_instance" "ec2" {
       volume_type = "gp2"
       delete_on_termination = true
     }
-    user_data = file("script.sh")
+
 }
