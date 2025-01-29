fca1a7c5eab5a2e1f57738f88f7a741d37601ce1
VINAY Y <vinay28623@gmail.com>
2025-01-29 18:08:31 +0530

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,21 +1,22 @@
 provider "aws" {
     region = "us-east-1"
-
+    access_key ="AKIASE5KRGE4ZFBVILTM"
+    secret_key ="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"
 }
+
 
 resource "aws_instance" "admin" {
     ami = "ami-0df8c184d5f6ae949"
     instance_type = "t2.medium"
     security_groups = [ "default" ]
-    key_name = "project"
+    key_name = "147"
     root_block_device {
       volume_size = 20
       volume_type = "gp2"
       delete_on_termination = true
     }
-    user_data = file("script.sh")
     tags = {
-      Name = "terraform"
+      Name = "Admin-server"
     }
 
 }
